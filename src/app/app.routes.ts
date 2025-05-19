50c66ea32848e14cfddebd2a53c7c571bd585c7d
Henrique <henriquedasilvaronzani@gmail.com>
2025-05-18 22:37:07 -0300

    Projeto finalizado!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {ChatComponent} from './chat/chat/chat.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'chat', title:'Chat', component: ChatComponent},
+  {path: '', redirectTo: '/chat', pathMatch: 'full' }
+];
---
+++ src/tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/**/*.{html,ts,js,jsx,tsx,vue}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default config
